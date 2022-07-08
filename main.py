b7045b3e1741ce50ae85954d1a8bde4d947ecd41
DNIIBOY <daniel@nettelfield.com>
2022-07-08 22:46:28 +0200

    started working on tkinter version

--- main.py
+++ main.py
@@ -1,75 +1,30 @@
-from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.properties import (
-    NumericProperty, ReferenceListProperty, ObjectProperty
-)
-from kivy.vector import Vector
-from kivy.clock import Clock
+from tkinter import *
+from PIL import ImageTk, Image, ImageGrab
 
 
-class PongPaddle(Widget):
-    score = NumericProperty(0)
+class InstaLocker:
+    def __init__(self):
+        self.main_window = Tk()
 
-    def bounce_ball(self, ball):
-        if self.collide_widget(ball):
-            vx, vy = ball.velocity
-            offset = (ball.center_y - self.center_y) / (self.height / 2)
-            bounced = Vector(-1 * vx, vy)
-            vel = bounced * 1.1
-            ball.velocity = vel.x, vel.y + offset
+    def start(self):
+        self.setup_main_window()
 
+    def setup_main_window(self):
+        self.main_window.title("Valorant Instalocker")
+        self.main_window.minsize(480, 270)
+        self.main_window.geometry("640x360")  # Default size
 
-class PongBall(Widget):
-    velocity_x = NumericProperty(0)
-    velocity_y = NumericProperty(0)
-    velocity = ReferenceListProperty(velocity_x, velocity_y)
+        # Setup background image
+        background_image = ImageTk.PhotoImage(file="img/iceboxBackground.png")
+        background_label = Label(self.main_window, image=background_image)
+        background_label.place(x=0, y=0, relwidth=1, relheight=1)
+        background_label.image = background_image  # Fixes issue with lost reference for image
 
-    def move(self):
-        self.pos = Vector(*self.velocity) + self.pos
-
-
-class PongGame(Widget):
-    ball = ObjectProperty(None)
-    player1 = ObjectProperty(None)
-    player2 = ObjectProperty(None)
-
-    def serve_ball(self, vel=(4, 0)):
-        self.ball.center = self.center
-        self.ball.velocity = vel
-
-    def update(self, dt):
-        self.ball.move()
-
-        # bounce of paddles
-        self.player1.bounce_ball(self.ball)
-        self.player2.bounce_ball(self.ball)
-
-        # bounce ball off bottom or top
-        if (self.ball.y < self.y) or (self.ball.top > self.top):
-            self.ball.velocity_y *= -1
-
-        # went of to a side to score point?
-        if self.ball.x < self.x:
-            self.player2.score += 1
-            self.serve_ball(vel=(4, 0))
-        if self.ball.right > self.width:
-            self.player1.score += 1
-            self.serve_ball(vel=(-4, 0))
-
-    def on_touch_move(self, touch):
-        if touch.x < self.width / 3:
-            self.player1.center_y = touch.y
-        if touch.x > self.width - self.width / 3:
-            self.player2.center_y = touch.y
-
-
-class PongApp(App):
-    def build(self):
-        game = PongGame()
-        game.serve_ball()
-        Clock.schedule_interval(game.update, 1.0 / 60.0)
-        return game
+        title = Label(self.main_window, text="Valorant Instalocker", fg="#ff4b50", font="Georgia 30", bg="#000000")
+        title.pack(pady=10)
 
 
 if __name__ == '__main__':
-    PongApp().run()
+    IL = InstaLocker()
+    IL.start()
+    IL.main_window.mainloop()
