630e8e8ac8c5cd42aec89a295611cb6b73052aad
DNIIBOY <daniel@nettelfield.com>
2022-07-09 18:38:50 +0200

    working buttons now ig

--- main.py
+++ main.py
@@ -27,14 +27,20 @@ AGENT_LIST = [
 
 class InstaLocker:
     def __init__(self):
-        self.main_window = Tk()
+        self.main_window = Tk()  # Root window
+
         # Setup background image
         background_image = ImageTk.PhotoImage(file="img/iceboxBackground.png")
         background_label = Label(self.main_window, image=background_image)
         background_label.place(x=0, y=0, relwidth=1, relheight=1)
         background_label.image = background_image  # Fixes issue with lost reference for image
 
-        self.agent_canvas = Canvas(self.main_window, height=200, width=600)
+        self.agent_canvas = Canvas(self.main_window, height=200, width=600)  # Grid of agents
+
+        self.unlocked_agents = []
+        self.selected_agent = ""
+
+        self.agent_button_list = []  # Contains button objects for all agents
 
     def start(self):
         self.setup_main_window()
@@ -56,26 +62,38 @@ class InstaLocker:
         )
 
         i = 0
-        button_list = []
         for y in range(2):
             # Account for extra agent on top row, when there is and odd agent count
             odd_offset = int(len(AGENT_LIST) % 2 == 1 and y == 0)
             for x in range((len(AGENT_LIST) // 2) + odd_offset):
-                agent_button = Button(
-                    self.agent_canvas,
-                    text=AGENT_LIST[i],
-                    height=3,
-                    width=7,
-                    background="white",
-                    foreground="#ff4b50"
+                self.agent_button_list.append(
+                    Button(
+                        self.agent_canvas,
+                        text=AGENT_LIST[i],
+                        height=3,
+                        width=7,
+                        background="white",
+                        foreground="#ff4b50",
+                        command=(lambda i=i: self.select_agent(i)),
+                    )
                 )
 
-                button_list.append(agent_button)
-                button_list[i].grid(column=x, row=y, padx=1, pady=1)
+                self.agent_button_list[i].grid(column=x, row=y, padx=1, pady=1)
 
                 i += 1
 
         self.agent_canvas.pack(pady=25)
+
+    def select_agent(self, agent_num: int):
+        print(agent_num)
+        self.selected_agent = AGENT_LIST[agent_num]
+        for but in self.agent_button_list:
+            but.configure(
+                bg="white"
+            )
+        self.agent_button_list[agent_num].configure(
+            bg="black"
+        )
 
 
 if __name__ == '__main__':
