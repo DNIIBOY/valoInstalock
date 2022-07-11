95d07f93830aca641a9d8b1b08be8689388847dc
DNIIBOY <daniel@nettelfield.com>
2022-07-11 19:25:56 +0200

    Reformat original.py

--- original.py
+++ original.py
@@ -29,36 +29,36 @@ def agent_select():
         print(f"agent {slotInput} selected")
 
     if 1 <= slotInput <= 10:
-        agent_X = agent_X+85*(slotInput-1)
+        agent_X = agent_X + 85 * (slotInput - 1)
     elif slotInput > 10:
-        agent_X = agent_X+85*(slotInput-12)
+        agent_X = agent_X + 85 * (slotInput - 12)
         agent_Y = agent_Y + 85
 
 
 def wait_for_select():
     while True:
-        img = imggrab.grab(bbox=(841, 784, 841+1, 784+1))
-        img2 = imggrab.grab(bbox=(950, 866, 950+1, 866+1))
+        img = imggrab.grab(bbox=(841, 784, 841 + 1, 784 + 1))
+        img2 = imggrab.grab(bbox=(950, 866, 950 + 1, 866 + 1))
 
         rgb_pixel_value = img.getpixel((0, 0))
         rgb_pixel_value2 = img2.getpixel((0, 0))
 
-        if rgb_pixel_value == (255,255,255) and rgb_pixel_value2 == (234,238,178):
+        if rgb_pixel_value == (255, 255, 255) and rgb_pixel_value2 == (234, 238, 178):
             return
-        
+
 
 def lockin():
     auto.click(agent_X, agent_Y)
     auto.click(clicks=1)
     sleep(0.02)
-    auto.moveTo(1000,820)
-    auto.drag(-15,0,duration=0.02)
+    auto.moveTo(1000, 820)
+    auto.drag(-15, 0, duration=0.02)
     auto.click(clicks=2)
 
 
 def wait_for_buy():
     while True:
-        img = imggrab.grab(bbox=(1000, 1045, 1000+1, 1045+1)) # single tp charge
+        img = imggrab.grab(bbox=(1000, 1045, 1000 + 1, 1045 + 1))  # single tp charge
 
         rgb_pixel_value = img.getpixel((0, 0))
 
@@ -70,12 +70,12 @@ def yoru_buy():
     press_and_release("b")
     # auto.moveTo(467, 434) # frenzy
     # auto.click(467, 434) # frenzy
-    auto.moveTo(467, 550) # ghost
-    auto.click(467, 550) # ghost
+    auto.moveTo(467, 550)  # ghost
+    auto.click(467, 550)  # ghost
     auto.click(clicks=1)
     sleep(0.02)
-    auto.moveTo(950,904) # flash
-    auto.drag(-15,0,duration=0.02)
+    auto.moveTo(950, 904)  # flash
+    auto.drag(-15, 0, duration=0.02)
     auto.click(clicks=2)
     sleep(0.02)
     # auto.moveTo(590,883) # decoy
